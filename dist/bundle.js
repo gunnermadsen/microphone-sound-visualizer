4439962145a89b01c26f99cfc30476d0f393ef6a
gunnermadsen <gunner.madsen@outlook.com>
2020-03-12 13:03:55 -0700

    removed unused code. updated d3 imports. added comments to code to help describe what is happenning

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "69a850606937307b42d0";
+/******/ 	var hotCurrentHash = "819d77a21b3d15ef099e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -866,37 +866,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const d3 = __importStar(__webpack_require__(/*! d3 */ "./node_modules/d3/index.js"));
+const d3_1 = __webpack_require__(/*! d3 */ "./node_modules/d3/index.js");
 __webpack_require__(/*! ./main.css */ "./main.css");
 class WaveGenerator {
     constructor() {
+        // when the WaveGenerator class is instantiated, we want to:
+        // - setup the audio context using the microphone
+        // - setup an event listener on the '#start-waves' button 
         this.audioContext = null;
         this.analyser = null;
         this.mediaStream = null;
         this.source = null;
-        this.processor = null;
         this.windowWidth = window.innerWidth - 10;
         this.windowHeight = window.innerHeight - 100;
         this.frequencyData = null;
         this.frequencyBinCount = 128;
         this.maxStdAmplitude = 16;
-        this.xScaler = d3.scaleLinear().domain([0, this.frequencyBinCount - 1]).range([0, 200]);
-        this.yScaler = d3.scaleLinear().domain([0, this.maxStdAmplitude]).range([185, 0]);
         this.isListening = false;
+        this.xScaler = d3_1.scaleLinear().domain([0, this.frequencyBinCount - 1]).range([0, 200]);
+        this.yScaler = d3_1.scaleLinear().domain([0, this.maxStdAmplitude]).range([185, 0]);
         this.setMicrophoneAudioToContext();
         document.getElementById('start-waves').addEventListener('click', this.initializeAudioContext.bind(this));
     }
     setMicrophoneAudioToContext() {
         return __awaiter(this, void 0, void 0, function* () {
             try {
+                // create a media stream object from the getUserMedia api
                 this.mediaStream = yield navigator.mediaDevices.getUserMedia({ audio: true, video: false });
             }
             catch (error) {
@@ -905,16 +901,16 @@ class WaveGenerator {
         });
     }
     initializeAudioContext() {
+        // create the audiocontext objet, configure the source, and the analyser 
+        // the analyser will be responsible for intercepting the audio data from the source in the 'node graph'
+        // and creating a data structure that can be used for visualizing the audio waves
         if (!this.isListening) {
             this.audioContext = new AudioContext();
             this.source = this.audioContext.createMediaStreamSource(this.mediaStream);
             this.analyser = this.audioContext.createAnalyser();
-            // this.processor = this.audioContext.createScriptProcessor(1024, 1, 1)
-            // this.source.connect(this.processor)
+            // connect the analyser to the source
             this.source.connect(this.analyser);
-            // this.processor.connect(this.audioContext.destination)
             this.analyser.connect(this.audioContext.destination);
-            // this.processor.onaudioprocess = this.generateGraphFromAudio.bind(this)
             this.isListening = true;
             this.initChart();
         }
@@ -923,56 +919,55 @@ class WaveGenerator {
         }
     }
     initChart() {
+        // create a typed array from the analyser frequency data
         this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
         this.analyser.getByteFrequencyData(this.frequencyData);
-        this.colorScale = d3.scaleLinear()
+        // setup the color scale function for adding style to the graph
+        this.colorScale = d3_1.scaleLinear()
             .domain([0, 150])
             .range((["purple", "red", "green"]));
         // this.colorScale = d3.scaleSequential(d3.interpolateYlGnBu).domain(d3.extent(this.frequencyData, (d: any) => d.value))
-        this.circleX = d3.scaleLinear()
-            .domain([0, this.frequencyData.length])
-            .range([0, this.windowWidth]);
-        this.svg = d3.select('#wave-container').append('svg')
+        // initialize the svg element and append it to the DOM
+        this.svg = d3_1.select('#wave-container').append('svg')
             .attr('width', this.windowWidth)
             .attr('height', 250)
             .classed('graph__outline', true);
+        // setup the width of the graph using the x and y scaler function declared above
         const w = this.xScaler(1) - this.xScaler(0);
+        // define the radius on the x axis
         const rx = w * 0.1;
-        // logic for rectangles
-        let rects = this.svg.selectAll('rect')
+        // since the visualizations consist of many small rectangles, we want to style them relative to the typed array data 
+        this.svg.selectAll('rect')
             .data(this.frequencyData).enter().append('rect')
-            .style('fill', (datum, index) => this.colorScale(datum))
-            // .style('transition-timing-function', 'linear')
-            // .style('transition-duration', '5ms')
-            .attr('width', () => w) // this.windowWidth / data.length + .3
+            // style the rectangles based on the datum returned from the arrow function
+            .style('fill', datum => this.colorScale(datum))
+            // set rectangle element attribtues
+            .attr('width', () => w)
             .attr('rx', rx)
-            .attr('x', (datum, index) => this.xScaler(index))
-            .attr('y', (datum, index) => this.yScaler(datum))
-            .attr('height', (datum) => (this.yScaler(0) - this.yScaler(datum))) //
+            .attr('x', (_, index) => this.xScaler(index))
+            .attr('y', datum => this.yScaler(datum))
+            .attr('height', datum => (this.yScaler(0) - this.yScaler(datum)))
             .attr('opacity', 0)
             .transition()
             .duration(0)
-            .ease(d3.easeLinear)
+            .ease(d3_1.easeLinear)
             .attr('opacity', 0.6);
+        // setup a function to draw the chart
         const drawChart = () => {
+            // asynchronously call the drawChart recursively when the animation needs to be redrawn 
             requestAnimationFrame(drawChart);
             this.analyser.getByteFrequencyData(this.frequencyData);
+            // redraw the rectangles in the graph
             this.svg.selectAll('rect')
                 .data(this.frequencyData)
                 .transition()
                 .duration(5)
-                .ease(d3.easeLinear)
-                .style('fill', (datum, index) => this.colorScale(datum))
-                .attr('y', (datum, index) => this.windowHeight / 4 - datum)
-                .attr('height', (datum, index) => (this.yScaler(0) - this.yScaler(datum)));
+                .ease(d3_1.easeLinear)
+                .style('fill', datum => this.colorScale(datum))
+                .attr('y', datum => this.windowHeight / 4 - datum)
+                .attr('height', datum => (this.yScaler(0) - this.yScaler(datum)));
         };
         drawChart();
-    }
-    initializeUserMediaListeners() {
-        this.mediaStream.onaddtrack = this.addMediaStreamTrack.bind(this);
-    }
-    addMediaStreamTrack(event) {
-        console.log(event);
     }
 }
 const generator = new WaveGenerator();
--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
-import * as d3 from 'd3'
+import { scaleLinear, select, easeLinear, ScaleLinear, } from 'd3'
 import './main.css'
 class WaveGenerator {
-
     private audioContext: AudioContext = null
     private analyser: AnalyserNode = null
     private mediaStream: MediaStream = null
     private source: MediaStreamAudioSourceNode = null
-    private processor: ScriptProcessorNode = null
     public windowWidth: number = window.innerWidth - 10
     public windowHeight: number = window.innerHeight - 100
     private frequencyData: Uint8Array = null
-    private colorScale: any
-    private circleX: any
+    private colorScale: ScaleLinear<number, number>
     private readonly frequencyBinCount = 128
     private readonly maxStdAmplitude = 16
-    private xScaler = d3.scaleLinear().domain([0, this.frequencyBinCount - 1]).range([0, 200])
-    private yScaler = d3.scaleLinear().domain([0, this.maxStdAmplitude]).range([185, 0])
+    private xScaler: ScaleLinear<number, number>
+    private yScaler: ScaleLinear<number, number>
     private svg: any
     private isListening: boolean = false
 
     constructor() {
+        // when the WaveGenerator class is instantiated, we want to:
+        // - setup the audio context using the microphone
+        // - setup an event listener on the '#start-waves' button 
+
+        this.xScaler = scaleLinear().domain([0, this.frequencyBinCount - 1]).range([0, 200])
+        this.yScaler = scaleLinear().domain([0, this.maxStdAmplitude]).range([185, 0])
         this.setMicrophoneAudioToContext()
         document.getElementById('start-waves').addEventListener('click', this.initializeAudioContext.bind(this))
     }
@@ -27,6 +30,7 @@ class WaveGenerator {
     private async setMicrophoneAudioToContext() {
         try {
 
+            // create a media stream object from the getUserMedia api
             this.mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false })
 
         }
@@ -39,20 +43,19 @@ class WaveGenerator {
 
     private initializeAudioContext() {
 
+        // create the audiocontext objet, configure the source, and the analyser 
+        // the analyser will be responsible for intercepting the audio data from the source in the 'node graph'
+        // and creating a data structure that can be used for visualizing the audio waves
         if (!this.isListening) {
             this.audioContext = new AudioContext()
     
             this.source = this.audioContext.createMediaStreamSource(this.mediaStream)
             this.analyser = this.audioContext.createAnalyser()
-            // this.processor = this.audioContext.createScriptProcessor(1024, 1, 1)
-    
-            // this.source.connect(this.processor)
+
+            // connect the analyser to the source
             this.source.connect(this.analyser)
-            // this.processor.connect(this.audioContext.destination)
-    
+
             this.analyser.connect(this.audioContext.destination)
-    
-            // this.processor.onaudioprocess = this.generateGraphFromAudio.bind(this)
             this.isListening = true
 
             this.initChart()
@@ -62,71 +65,69 @@ class WaveGenerator {
     }
 
     private initChart() {
-        this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount)
 
+        // create a typed array from the analyser frequency data
+        this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount)
         this.analyser.getByteFrequencyData(this.frequencyData)
 
-        this.colorScale = d3.scaleLinear()
+        // setup the color scale function for adding style to the graph
+        this.colorScale = scaleLinear()
             .domain([0, 150])
             .range((["purple", "red", "green"]) as any)
 
         // this.colorScale = d3.scaleSequential(d3.interpolateYlGnBu).domain(d3.extent(this.frequencyData, (d: any) => d.value))
 
-        this.circleX = d3.scaleLinear()
-            .domain([0, this.frequencyData.length])
-            .range([0, this.windowWidth])
-
-        this.svg = d3.select('#wave-container').append('svg')
+        // initialize the svg element and append it to the DOM
+        this.svg = select('#wave-container').append('svg')
             .attr('width', this.windowWidth)
             .attr('height', 250)
             .classed('graph__outline', true)
 
+        // setup the width of the graph using the x and y scaler function declared above
         const w = this.xScaler(1) - this.xScaler(0)
-        const rx = w * 0.1
-        // logic for rectangles
-        let rects = this.svg.selectAll('rect')
-            .data(this.frequencyData).enter().append('rect')
-            .style('fill', (datum: any, index: any) => this.colorScale(datum))
-            // .style('transition-timing-function', 'linear')
-            // .style('transition-duration', '5ms')
-            .attr('width', () => w) // this.windowWidth / data.length + .3
-            .attr('rx', rx)
-            .attr('x', (datum: any, index: any) => this.xScaler(index))
-            .attr('y', (datum: any, index: any) => this.yScaler(datum))
-            .attr('height', (datum: any) => (this.yScaler(0) - this.yScaler(datum))) //
-            .attr('opacity', 0)
-            .transition()
-            .duration(0)
-            .ease(d3.easeLinear)
-            .attr('opacity', 0.6)
 
+        // define the radius on the x axis
+        const rx = w * 0.1
+
+        // since the visualizations consist of many small rectangles, we want to style them relative to the typed array data 
+        this.svg.selectAll('rect')
+            .data(this.frequencyData).enter().append('rect')
+            // style the rectangles based on the datum returned from the arrow function
+            .style('fill', datum => this.colorScale(datum))
+
+            // set rectangle element attribtues
+                .attr('width', () => w)
+                .attr('rx', rx)
+                .attr('x', (_: any, index: any) => this.xScaler(index))
+                .attr('y', datum => this.yScaler(datum))
+                .attr('height', datum => (this.yScaler(0) - this.yScaler(datum)))
+                .attr('opacity', 0)
+            .transition()
+                .duration(0)
+                    .ease(easeLinear)
+                        .attr('opacity', 0.6)
+
+        // setup a function to draw the chart
         const drawChart = () => {
+            // asynchronously call the drawChart recursively when the animation needs to be redrawn 
             requestAnimationFrame(drawChart)
+
 
             this.analyser.getByteFrequencyData(this.frequencyData)
 
+            // redraw the rectangles in the graph
             this.svg.selectAll('rect')
                 .data(this.frequencyData)
-                .transition()
-                .duration(5)
-                .ease(d3.easeLinear)
-                .style('fill', (datum: any, index: any) => this.colorScale(datum))
-
-                .attr('y', (datum: any, index: any) => this.windowHeight / 4 - datum)
-                .attr('height', (datum: any, index: any) => (this.yScaler(0) - this.yScaler(datum)))
+                    .transition()
+                        .duration(5)
+                            .ease(easeLinear)
+                .style('fill', datum => this.colorScale(datum))
+                    .attr('y', datum => this.windowHeight / 4 - datum)
+                    .attr('height', datum => (this.yScaler(0) - this.yScaler(datum)))
         }
 
         drawChart()
         
-    }
-
-
-    private initializeUserMediaListeners() {
-        this.mediaStream.onaddtrack = this.addMediaStreamTrack.bind(this)
-    }
-
-    private addMediaStreamTrack(event: any) { // MediaStreamTrackEvent
-        console.log(event)
     }
 
 }
